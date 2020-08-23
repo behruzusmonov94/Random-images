d8d33b25ec520007e32eda12a60798ea0d137bae
UBEHRUZ <behruzusmonov94@gmail.com>
2020-08-23 17:17:28 +0500

    random images

---
+++ app.js
@@ -0,0 +1,96 @@
+const modal = document.getElementById('modal');
+
+
+const showImg = document.getElementById('showImg');
+
+function clickPict(img) {
+
+    $('.ui.modal')
+    .modal('show')
+  ;
+showImg.innerHTML = `
+
+<div class="actions">
+    <div class="ui red basic cancel inverted button">
+        <i class="remove icon"></i>
+       close
+    </div>
+    
+</div>
+<div class="content">
+    <img src="${img.src}">
+</div>
+
+
+`
+    
+
+
+
+}
+
+
+
+
+
+const container = document.getElementById('container');
+
+for (let i = 1; i < 30; i++) {
+    let randomX = Math.floor(Math.random() * 500) + 400;
+    let randomY = Math.floor(Math.random() * 500) + 400;
+
+    container.innerHTML += `
+    <div class="box"> 
+        <img src="https://picsum.photos/${randomX}/${randomY}" alt=""  onclick="clickPict(this)">
+    </div>
+    `
+
+    // console.log(randomX);
+
+
+}
+
+const loadMore = document.getElementById('loadMore');
+
+loadMore.addEventListener('click', ()=>{
+    for (let i = 1; i < 10; i++) {
+        let randomX = Math.floor(Math.random() * 800) + 500;
+        let randomY = Math.floor(Math.random() * 800) + 500;
+    
+        container.innerHTML += `
+        <div class="box"> 
+            <img src="https://picsum.photos/${randomX}/${randomY}" alt=""  onclick="clickPict(this)">
+        </div>
+        `
+
+    }
+})
+// window.addEventListener('scroll', ()=>{
+//     let mainScroll = 600;
+//     if(window.scrollY > mainScroll){
+//         // for (let i = 1; i < 2; i++) {
+//         //     let randomX = Math.floor(Math.random() * 800) + 500;
+//         //     let randomY = Math.floor(Math.random() * 800) + 500;
+        
+//         //     container.innerHTML += `
+//         //     <div class="box"> 
+//         //         <img src="https://picsum.photos/${randomX}/${randomY}" alt=""  onclick="clickPict(this)">
+//         //     </div>
+//         //     `
+//         // }
+//         mainScroll = mainScroll+600;
+//     }
+//     console.log(mainScroll)
+// })
+
+
+
+const modalOpen = document.getElementById('modalOpen')
+
+// modalOpen.addEventListener('click', ()=>{
+
+//     $('.ui.modal')
+//   .modal('show')
+// ;
+
+// })
